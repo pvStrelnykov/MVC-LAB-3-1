0450a1b1e66d0a500982d4ffb405f137f32d717f
StrelnikovPavlo <strelnikovpavel82@gmail.com>
2024-04-15 19:19:38 +0200

    zadanie 5

---
+++ zad5/app.js
@@ -0,0 +1,26 @@
+const http = require('http')
+const student = require('./student')
+
+const PORT = 3000
+
+const app = http.createServer((req, res) => {
+	res.setHeader('Content-Type', 'text/html')
+	res.write(`
+		<!DOCTYPE html>
+			<html lang="en">
+			<head>
+				<meta charset="UTF-8">
+				<meta name="viewport" content="width=device-width, initial-scale=1.0">
+				<title>About student</title>
+			</head>
+			<body>
+				<h1>${`My name is ${student.getStudentFullName()}. My student ID is ${student.getStudentId()}`}</h1>
+			</body>
+		</html>
+	`)
+	res.end()
+})
+
+app.listen(PORT, () => {
+	console.log(`Server is running on ${PORT}`)
+})
