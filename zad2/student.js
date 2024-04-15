280e748109ce5141eed676ab366e13fd9800dd3e
StrelnikovPavlo <strelnikovpavel82@gmail.com>
2024-04-15 18:59:00 +0200

    zadanie 2

---
+++ zad2/student.js
@@ -0,0 +1,8 @@
+
+const FULLNAME = 'Pavlo Strelnykov'
+const STUDENT_ID = 44981
+
+const getStudentFullName = () => FULLNAME
+const getStudentId = () => STUDENT_ID
+
+console.log(`My name is ${getStudentFullName()}. My student ID is ${getStudentId()}`)
